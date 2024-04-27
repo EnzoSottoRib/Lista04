5dd745cf0278438c0fbc3e16c625a1b630ef09f5
Ana Paula Endler <anapaulaendler@gmail.com>
2024-04-27 10:42:50 -0300

    ex01

--- Biblioteca/src/Programa.java
+++ Biblioteca/src/Programa.java
@@ -1,5 +1,30 @@
+import br.edu.up.Livro;
+
 public class Programa {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        String codigo = "1598FHK";
+        String titulo = "Core Java 2";
+        String[] autores = {"Cay S. Horstmann", "Gary Cornell"};
+        String isbn = "0130819336";
+        int ano = 2005;
+
+        Livro livro01 = new Livro(codigo, titulo, autores, isbn, ano);
+
+        Livro livro02 = new Livro();
+
+        livro02.setCodigo("9865PLO");
+        livro02.setTitulo("Java, Como programar");
+        String[] autoresLivro02 = {"Harvey Deitel"};
+        livro02.setAutores(autoresLivro02);
+        livro02.setIsbn("0130341517");
+        livro02.setAno(2015);
+
+        Prompt.imprimir("Detalhes do livro 01:");
+        Prompt.imprimir(livro01.toString());
+
+        Prompt.imprimir("Detalhes do livro 02:");
+        Prompt.imprimir(livro02.toString());
+
     }
 }
