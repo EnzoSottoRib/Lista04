77e1f54ab6fb71a3653a6cd6f27de47819a941a3
Ana Paula Endler <anapaulaendler@gmail.com>
2024-05-01 11:00:01 -0300

    comecando o dia

--- Agenda (EX03)/src/br/edu/up/Dia.java
+++ Agenda (EX03)/src/br/edu/up/Dia.java
@@ -1,22 +1,32 @@
 package br.edu.up;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Dia {
     
     int diaMes;
-    Compromisso[] compromisso;
+    List<Compromisso> compromisso;
 
     Dia(int diaMes) {
-
+        this.diaMes = diaMes;
+        this.compromisso = new ArrayList<>();
     }
 
     public void adicionarCompromisso(Compromisso comp) {
-
+        compromisso.add(comp); 
+        System.out.println("Compromisso adicionado com sucesso! :)");
     }
 
-    // Não sei se é void, conferir com o Enzo
-
     public void consultarCompromisso(int hora) {
-
+        for (Compromisso comp : compromisso) {
+            if (comp.getHora() == hora) { 
+                System.out.println("Compromisso encontrado: ");
+                System.out.println(comp); 
+                return; 
+            }
+        }
+        System.out.println("Nenhum compromisso encontrado para o horário mencionado. ");
     }
 
     public void excluirCompromisso (int hora) {
