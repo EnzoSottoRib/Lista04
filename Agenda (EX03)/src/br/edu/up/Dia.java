94c93d48b99de7e659e280d82c35b5fd13b3950f
Ana Paula Endler <anapaulaendler@gmail.com>
2024-05-01 13:04:34 -0300

    terminando o Dia

--- Agenda (EX03)/src/br/edu/up/Dia.java
+++ Agenda (EX03)/src/br/edu/up/Dia.java
@@ -7,6 +7,7 @@ public class Dia {
     
     int diaMes;
     List<Compromisso> compromisso;
+    // ana: decidi usar uma lista porque em um vetor o seu tamanho não pode ser alterado
 
     Dia(int diaMes) {
         this.diaMes = diaMes;
@@ -20,6 +21,7 @@ public class Dia {
 
     public void consultarCompromisso(int hora) {
         for (Compromisso comp : compromisso) {
+            // ana: o enhanced for loop é mais prático
             if (comp.getHora() == hora) { 
                 System.out.println("Compromisso encontrado: ");
                 System.out.println(comp); 
@@ -30,15 +32,24 @@ public class Dia {
     }
 
     public void excluirCompromisso (int hora) {
-
+        for (Compromisso comp : compromisso) {
+            if (comp.getHora() == hora) {
+                compromisso.remove(comp);
+                System.out.println("Compromisso removido com sucesso! :)");
+            }
+        }
+        System.out.println("Nenhum compromisso encontrado para o horário mencionado. ");
     }
 
     public String listarCompromissos() {
-                
-        String teste = "Oi";
-
-        return teste;
-
+        StringBuilder sb = new StringBuilder();
+        // ana: preferi usar o stringbuilder (e aprender o que é haha) ao string porque ele é mais prático, mutável, e administra melhor a memória :)
+        for (Compromisso comp : compromisso) {
+            sb.append(comp.toString()).append("\n");
+            // ana: explicando o cógido: o append adiciona e vai concatenando (juntando) as infos (atributos) dos compromissos! o \n é pra quebrar a linha!
+        }
+        return sb.toString(); 
     }
+    
     
 }
