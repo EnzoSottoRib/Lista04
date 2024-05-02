e158f880384cbc94a78452377cf4bca9d8fe7e52
enzosottorib <enzonador1@gmail.com>
2024-05-02 17:10:01 -0300

    alterações nas classes e inserção da classe menu

--- Estacionamento (EX04)/src/br/edu/up/Estacionamento.java
+++ Estacionamento (EX04)/src/br/edu/up/Estacionamento.java
@@ -1,35 +1,58 @@
 package br.edu.up;
 
 public class Estacionamento {
-    private int vagas;
+    private Carro[] vagas;
     private int entradas;
     private int saidas;
     private double valorPeriodo;
+    private int vagasLivres;
 
     public Estacionamento(){
     }
 
-    public Estacionamento(int vagas, int entradas, int saidas, double valorPeriodo) {
-        this.vagas = vagas;
+    public Estacionamento(int numVagas, int entradas, int saidas, double valorPeriodo, int vagasLivres) {
+        this.vagas = new Carro[10];
         this.entradas = entradas;
         this.saidas = saidas;
         this.valorPeriodo = valorPeriodo;
-    }
-
-    public void ocuparVaga(){
+        this.vagasLivres = 10;
 
     }
 
-    public void verificarVagas(){
-
-    }
-
-    public void buscarCarro(){
-
-    }
-
-    public void emitirRelatorio(){
+    public String ocuparVaga(Carro carro){
         
+        for(int i = 0; i < 10; i++){
+            if (vagas[i] == null){
+                vagas[i] = carro;
+                vagasLivres --;
+                entradas ++;
+                return "Carro adicionado na vaga" + i;
+            }
+        }
+        return "Nenhuma vaga disponível!";
+    }
+
+    public String verificarVagas(){
+        return "Existem " + vagasLivres + " vagas livres!";
+    }
+
+    public String desocuparVaga(String placa){
+        for (int i = 0; i < 10; i++){
+            if (vagas[i] != null && vagas[i].getPlaca().equalsIgnoreCase(placa)){
+                vagas[i] = null;
+                vagasLivres++;
+                saidas ++;
+                return "Carro com placa " + placa + " removido da vaga!";
+            }
+        }
+        return "Carro com placa " + placa + " não encontrado.";
+    }
+
+    public String emitirRelatorio(){
+        valorPeriodo = entradas * 5;
+        return entradas + " carros entraram.\n" + 
+        saidas + " carros sairam.\n" + 
+        "Valor de pagamentos no período: R$" + valorPeriodo; 
     }
 
 }
