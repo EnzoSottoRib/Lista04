f619ee9ae3b8430bab628ccbf28f3c312da48346
Your Name <you@example.com>
2024-05-02 15:26:05 -0300

    criação das classes

--- Estacionamento (EX04)/src/Programa.java
+++ Estacionamento (EX04)/src/Programa.java
@@ -1,4 +1,4 @@
-public class App {
+public class Programa {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
---
+++ Estacionamento (EX04)/src/br/edu/up/Estacionamento.java
@@ -0,0 +1,35 @@
+package br.edu.up;
+
+public class Estacionamento {
+    private int vagas;
+    private int entradas;
+    private int saidas;
+    private double valorPeriodo;
+
+    public Estacionamento(){
+    }
+
+    public Estacionamento(int vagas, int entradas, int saidas, double valorPeriodo) {
+        this.vagas = vagas;
+        this.entradas = entradas;
+        this.saidas = saidas;
+        this.valorPeriodo = valorPeriodo;
+    }
+
+    public void ocuparVaga(){
+
+    }
+
+    public void verificarVagas(){
+
+    }
+
+    public void buscarCarro(){
+
+    }
+
+    public void emitirRelatorio(){
+        
+    }
+
+}
