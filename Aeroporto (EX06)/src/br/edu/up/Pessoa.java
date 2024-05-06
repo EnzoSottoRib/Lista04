5bae6e30e1b9bd79fe9bdeea1194df32974ae4b3
enzosottorib <enzonador1@gmail.com>
2024-05-06 20:48:32 -0300

    criação da classe Pessoa

---
+++ Aeroporto (EX06)/src/br/edu/up/Pessoa.java
@@ -0,0 +1,9 @@
+package br.edu.up;
+
+public class Pessoa {
+    private String nome;
+    private String rg;
+    private String codigoAeronave;
+    private String tipoAeronave;
+    private int quantidadeAssentos;
+}
