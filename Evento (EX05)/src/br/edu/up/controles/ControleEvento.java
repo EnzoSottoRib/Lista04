bf09bc2ea7d35f624e43e18d1e493bd3388141d5
Ana Paula Endler <anapaulaendler@gmail.com>
2024-05-07 10:11:45 -0300

    controles

--- Evento (EX05)/src/br/edu/up/controles/ControleEvento.java
+++ Evento (EX05)/src/br/edu/up/controles/ControleEvento.java
@@ -1,6 +1,18 @@
 package br.edu.up.controles;
+import br.edu.up.*;
 
 public class ControleEvento {
     
+    public void criarEvento() {
+
+    }
+
+    public void editarEvento() {
+
+    }
+
+    public void excluirEvento() {
+
+    }
     
 }
--- Evento (EX05)/src/br/edu/up/controles/ControleReserva.java
+++ Evento (EX05)/src/br/edu/up/controles/ControleReserva.java
@@ -1,5 +1,17 @@
 package br.edu.up.controles;
 
 public class ControleReserva {
-    
+     
+    public void criarReserva() {
+
+    }
+
+    public void editarReserva() {
+
+    }
+
+    public void excluirReserva() {
+
+    }
+       
 }
