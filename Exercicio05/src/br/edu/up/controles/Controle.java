b8b04315207f830c6e47cfc6066873bf5e103166
Ana Paula Endler <anapaulaendler@gmail.com>
2024-05-10 11:36:18 -0300

    refazendo o cinco (retirando listas)

--- Exercicio05/src/br/edu/up/controles/Controle.java
+++ Exercicio05/src/br/edu/up/controles/Controle.java
@@ -1,13 +1,14 @@
 package br.edu.up.controles;
-import java.util.List;
 
 import br.edu.up.modelos.Evento;
 import br.edu.up.modelos.Reserva;
 
 public class Controle {
 
-    private List<Evento> listaEventos;
-    private List<Reserva> listaReservas;
+    Evento listaEventos[] = new Evento[100];
+    Reserva listaReservas[] = new Reserva[100];
+    int iE = 0;
+    int iR = 0;
 
     public void criarEvento() {
 
@@ -46,54 +47,55 @@ public class Controle {
         evento.setIngressosVendidos(Prompt.lerInteiro("Digite a quantidade de ingressos vendidos: "));
         evento.setPrecoIngresso(Prompt.lerDecimal("Digite o preço individual dos ingressos, em reais: "));
     
-        listaEventos.add(evento);
+        listaEventos[iE] = evento;
+        iE++;
     
     }
 
     public void editarNome(int b) {
-        listaEventos.get(b-1).setNomeEvento(Prompt.lerLinha("Digite o nome atualizado do evento: "));
+        listaEventos[b-1].setNomeEvento(Prompt.lerLinha("Digite o nome atualizado do evento: "));
     }
 
     public void editarLocal(int b) {
-        listaEventos.get(b-1).setLocalEvento(Prompt.lerLinha("Digite o novo local do evento: "));
+        listaEventos[b-1].setLocalEvento(Prompt.lerLinha("Digite o novo local do evento: "));
     }
 
     public void editarHora(int b) {
         do {
-            listaEventos.get(b-1).setHora(Prompt.lerInteiro("Digite o horário no formato 24H: "));
-        } while  (listaEventos.get(b-1).getHora() < 0 && listaEventos.get(b-1).getHora() > 24);
+            listaEventos[b-1].setHora(Prompt.lerInteiro("Digite o horário no formato 24H: "));
+        } while  (listaEventos[b-1].getHora() < 0 && listaEventos[b-1].getHora() > 24);
     }
 
     public void editarDia(int b) {
-        listaEventos.get(b-1).setDia(Prompt.lerInteiro("Digite o novo dia (seu número, no formato 'DD'): "));
+        listaEventos[b-1].setDia(Prompt.lerInteiro("Digite o novo dia (seu número, no formato 'DD'): "));
     }
 
     public void editarMes(int b) {
-        listaEventos.get(b-1).setMes(Prompt.lerInteiro("Digite o novo mês (seu número, no formato 'MM'): "));
+        listaEventos[b-1].setMes(Prompt.lerInteiro("Digite o novo mês (seu número, no formato 'MM'): "));
     }
 
     public void editarAno(int b) {
-        listaEventos.get(b-1).setAno(Prompt.lerInteiro("Digite o novo ano (seu número, no formato 'AAAA'): "));
+        listaEventos[b-1].setAno(Prompt.lerInteiro("Digite o novo ano (seu número, no formato 'AAAA'): "));
     }
 
     public void editarLotacaoMax(int b) {
-        listaEventos.get(b-1).setLotacaoMax(Prompt.lerInteiro("Digite a nova lotação máxima: "));
+        listaEventos[b-1].setLotacaoMax(Prompt.lerInteiro("Digite a nova lotação máxima: "));
     }
 
     public void editarIngressosVendidos (int b) {
-        listaEventos.get(b-1).setIngressosVendidos(Prompt.lerInteiro("Digite a nova quantidade de ingressos vendidos: "));
+        listaEventos[b-1].setIngressosVendidos(Prompt.lerInteiro("Digite a nova quantidade de ingressos vendidos: "));
     }
 
     public void editarPrecoIngressos (int b) {
-        listaEventos.get(b-1).setPrecoIngresso(Prompt.lerDecimal("Digite o novo preço do ingresso, em reais: "));
+        listaEventos[b-1].setPrecoIngresso(Prompt.lerDecimal("Digite o novo preço do ingresso, em reais: "));
     }
 
     public void excluirEvento(int b) {
-        listaEventos.remove(listaEventos.get(b-1));
+        listaEventos[b-1] = null;
     }
 
     public String listarEvento() {
-        if (listaEventos.isEmpty()) {
+        if (listaEventos == null) {
             return "Não há eventos listados.";
         } else {
             StringBuilder sb = new StringBuilder();
@@ -115,33 +117,40 @@ public class Controle {
 
         int n = selecionarEvento();
 
-        reserva.setAno(listaEventos.get(n-1).getAno());
-        reserva.setMes(listaEventos.get(n-1).getMes());
-        reserva.setDia(listaEventos.get(n-1).getDia());
-        reserva.setHora(listaEventos.get(n-1).getHora());
-        reserva.setPrecoIngresso(listaEventos.get(n-1).getPrecoIngresso());
+        reserva.setAno(listaEventos[n-1].getAno());
+        reserva.setMes(listaEventos[n-1].getMes());
+        reserva.setDia(listaEventos[n-1].getDia());
+        reserva.setHora(listaEventos[n-1].getHora());
+        reserva.setPrecoIngresso(listaEventos[n-1].getPrecoIngresso());
 
         reserva.setNomeResponsavel(Prompt.lerLinha("Digite o nome do responsável: "));
-        reserva.setQtdePessoas(Prompt.lerInteiro("Digite a quantidade de pessoas: "));
+        do {
+            reserva.setQtdePessoas(Prompt.lerInteiro("Digite a quantidade de pessoas: "));
+            if (listaEventos[n-1].getLotacaoMax() > (listaEventos[n-1].getIngressosVendidos() + reserva.getQtdePessoas())) {
+                Prompt.imprimir("Não há ingressos suficientes.");
+            }
+        } while (listaEventos[n-1].getLotacaoMax() <= (listaEventos[n-1].getIngressosVendidos() + reserva.getQtdePessoas()));
 
-        listaReservas.add(reserva);
-
-    }
+        reserva.setValorTotal(listaEventos[n-1].getPrecoIngresso() * reserva.getQtdePessoas());
+        
+        listaReservas[iR] = reserva;
+        iR++;
+    }        
 
     public void editarNomeResponsavel(int b) {
-        listaReservas.get(b-1).setNomeResponsavel(Prompt.lerLinha("Digite o nome do responsável: "));
+        listaReservas[b-1].setNomeResponsavel(Prompt.lerLinha("Digite o nome do responsável: "));
     }
 
     public void editarQtdePessoas(int b) {
-        listaReservas.get(b-1).setQtdePessoas(Prompt.lerInteiro("Digite a quantidade de pessoas: "));
+        listaReservas[b-1].setQtdePessoas(Prompt.lerInteiro("Digite a quantidade de pessoas: "));
     }
 
     public void excluirReserva(int b) {
-        listaReservas.remove(listaReservas.get(b-1));
+        listaReservas[b-1] = null;
     }
 
     public String listarReserva() {
-        if (listaReservas.isEmpty()) {
+        if (listaReservas == null) {
             return "Não há reservas.";
         } else {
             StringBuilder sb = new StringBuilder();
