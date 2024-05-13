24aed16b1ec7039115b61567fa302666921fa6d4
Ana Paula Endler <anapaulaendler@gmail.com>
2024-05-13 13:07:33 -0300

    fim do pomodoro

--- Exercicio07/src/br/edu/up/controles/Controle.java
+++ Exercicio07/src/br/edu/up/controles/Controle.java
@@ -6,7 +6,9 @@ import br.edu.up.Prompt;
 public class Controle {
     
     Aluno vetorAluno[] = new Aluno[600];
+
     Prof vetorProf[] = new Prof[30];
+    Titulacao titulacao;
     Disciplina vetorDisciplina[] = new Disciplina[30];
 
     int a = 0;
@@ -18,6 +20,30 @@ public class Controle {
         aluno.setNome(Prompt.lerLinha("Digite o nome do aluno: "));
         aluno.setRg(Prompt.lerLinha("Digite o RG do aluno: "));
         aluno.setMatricula(Prompt.lerLinha("Digite a matrícula do aluno: "));
+
+        vetorAluno[a] = aluno;
+        a++;
+    }
+
+    public void adicionarProf() {
+        Prof prof = new Prof();
+        prof.setNome(Prompt.lerLinha("Digite o nome do professor: "));
+        prof.setRg(Prompt.lerLinha("Digite o RG do professor: "));
+        prof.setMatricula(Prompt.lerLinha("Digite a matrícula do professor: "));
+        prof.setnLattes(Prompt.lerInteiro("Digite o número Lattes do professor: "));
+
+        Prompt.imprimir("Agora vamos registrar sua titulação: ");
+
+        Titulacao titulacao = new Titulacao();
+        titulacao.setNomeInstituicao(Prompt.lerLinha("Digite o nome da instituição: "));
+        titulacao.setAno(Prompt.lerInteiro("Digite o ano de obtenção da titulação: "));
+        titulacao.setTituloObtido(Prompt.lerLinha("Digite o título obtido: "));
+        titulacao.setTituloTCC(Prompt.lerLinha("Digite o título do TCC: "));
+    
+        prof.setTitulacao(titulacao);
+
+        vetorProf[p] = prof;
+        p++;
     }
 
 }
