468e9ab24904e6b65b4c2fb245d9cbeaa0c9182f
Ana Paula Endler <anapaulaendler@gmail.com>
2024-05-13 18:34:09 -0300

    complicando

--- Exercicio07/src/br/edu/up/modelos/Aluno.java
+++ Exercicio07/src/br/edu/up/modelos/Aluno.java
@@ -1,18 +1,45 @@
 package br.edu.up.modelos;
 
+import java.util.Arrays;
+
 public class Aluno extends Pessoa {
 
     Aprovado aprovado;
+    Disciplina[] disciplinas;
+    int contadorDisciplina;
 
     public enum Aprovado {
         APROVADO, REPROVADO, PENDENTE
     }
 
-    @Override
-    public String toString() {
-        return "Aluno [nome=" + nome + ", rg=" + rg + ", matricula="
-                + matricula + ", aprovado=" + aprovado + "]";
+    public Aprovado getAprovado() {
+        return aprovado;
     }
 
-    
+    public void setAprovado(Aprovado aprovado) {
+        this.aprovado = aprovado;
+    }
+
+    public Disciplina[] getDisciplinas() {
+        return disciplinas;
+    }
+
+    public void setDisciplinas(Disciplina[] disciplinas) {
+        this.disciplinas = disciplinas;
+    }
+
+    public int getContadorDisciplina() {
+        return contadorDisciplina;
+    }
+
+    public void setContadorDisciplina(int contadorDisciplina) {
+        this.contadorDisciplina = contadorDisciplina;
+    }
+
+    @Override
+    public String toString() {
+        return "Aluno [nome=" + nome + ", aprovado=" + aprovado + ", rg=" + rg + ", matricula=" + matricula
+                + ", disciplinas=" + Arrays.toString(disciplinas) + ", contadorDisciplina=" + contadorDisciplina + "]";
+    }
+
 }
